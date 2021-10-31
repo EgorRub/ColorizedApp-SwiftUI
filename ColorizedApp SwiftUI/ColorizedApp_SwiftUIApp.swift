b9c1ba56425c5702916bd7691c3ba50eb38be516
Egor Rubtsov <rubtsove777@gmail.com>
2021-10-31 17:35:10 +0300

    Initial Commit

---
+++ ColorizedApp SwiftUI/ColorizedApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedApp_SwiftUIApp.swift
+//  ColorizedApp SwiftUI
+//
+//  Created by Егор on 31.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
